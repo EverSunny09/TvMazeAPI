d3784689136e34b196746dcbc2b93dd6d53d5672
Sunny George <Sunny.George@stepstone.com>
2022-12-09 11:48:46 +0000

    Test Cases

---
+++ TvMaze.Tests.Acceptance/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TvMaze.Tests.Acceptance
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
